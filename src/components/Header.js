f4b98b8bdf4f49463d9f00f38f134c7703decdbf
tapkichvuong <huytruongminh44@gmail.com>
2023-03-29 16:55:26 +0700

    Revert "Revert "last day3""
    This reverts commit 670d26da3077528ff843582efb7d82ae87b54987.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import Constants from 'expo-constants';
-import React, {useState} from "react";
+import React from "react";
 import { Text, View, StyleSheet, Image } from "react-native";
 import SearchTab from './SearchTab'
 
@@ -8,7 +8,7 @@ const Header = () => {
     <View style={styles.container}>
       <View style = {styles.brand}>
         <Image style={styles.logo} source={require("../../assets/book.png")} />
-        <Text style = {{fontSize: 15} }> English - Viet Dictionary </Text>
+        <Text style = {{fontSize: 16, fontWeight: '500'}}> English - Viet Dictionary </Text>
       </View>
       <SearchTab/>
     </View>
--- src/components/SearchTab.js
+++ src/components/SearchTab.js
@@ -1,32 +1,75 @@
 import React, { useState } from 'react';
 import { SearchBar } from '@rneui/base';
-import { View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Record from './Record';
+var data = require("../../data/anhviet.json");
 
 const SearchTab = () => {
   const [value, setValue] = useState("");
 
   return (
     <View>
-      <SearchBar
-        platform="default"
-        containerStyle={{ borderRadius: 15 }}
-        inputContainerStyle={{}}
-        inputStyle={{color: '#000'}}
-        leftIconContainerStyle={{}}
-        rightIconContainerStyle={{}}
-        lightTheme
-        loadingProps={{}}
-        onChangeText={newVal => setValue(newVal)}
-        onClearText={() => console.log(onClearText())}
-        placeholder="Search here..."
-        placeholderTextColor="#888"
-        cancelButtonTitle="Cancel"
-        cancelButtonProps={{}}
-        onCancel={() => console.log(onCancel())}
-        value={value}
-      />
+      <View style={{flexDirection: 'row'}}>
+        <SearchBar
+          platform="default"
+          containerStyle={{ borderRadius: 15 }}
+          inputStyle={{color: '#000'}}
+          lightTheme
+          loadingProps={{}}
+          onChangeText={newVal => setValue(newVal)}
+          onClearText={() => console.log(onClearText())}
+          placeholder="Search here..."
+          placeholderTextColor="#888"
+          cancelButtonTitle="Cancel"
+          onCancel={() => console.log(onCancel())}
+          value={value}
+        />
+        <Record
+          onSpeechEnd={(value) => {
+            setValue(value[0]);
+          }}
+          onSpeechStart={() => {
+            setValue("");
+          }}
+        />
+      </View>
+      <View style={styles.dropdown}>
+        {data.filter((item) =>{
+          const searchTerm = value.toLowerCase();
+          const word = item.tu.toLowerCase();
+          return (
+            searchTerm && word.startsWith(searchTerm) && word !== searchTerm
+          );
+        })
+        .slice(0, 5)
+        .map((item) => (
+          <View style={styles.drop_row} key={item.tu}>
+            <Text style={{padding: 10, color: '#fff'}}>
+              {item.tu}
+              <Text style={{padding: 10, fontStyle: 'italic', fontSize: 11, fontWeight: '200'}}>  {item.dang}</Text>
+            </Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
+
+
+const styles = StyleSheet.create({
+  dropdown: {
+    marginTop: 5,
+    flexDirection: 'column',
+    backgroundColor: '#548787',
+    borderRadius: 5,
+    zIndex: 1
+  },
+  drop_row: {
+    textAlign: 'left',
+    marginHorizontal: 10,
+    borderBottomColor: '#9EF78D',
+    borderBottomWidth: 0.6
+  }
+});
 
 export default SearchTab;
